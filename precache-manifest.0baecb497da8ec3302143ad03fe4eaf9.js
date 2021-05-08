38e7df5e6091471f0d9adeab59325125370ad955
shirinas2002 <83756732+shirinas2002@users.noreply.github.com>
2021-05-08 20:36:44 +0530

    Add files via upload

---
+++ precache-manifest.0baecb497da8ec3302143ad03fe4eaf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "37eacb164110fe257e29",
+    "url": "/static/css/main.abefbe7b.chunk.css"
+  },
+  {
+    "revision": "37eacb164110fe257e29",
+    "url": "/static/js/main.37eacb16.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6e4859109c81c7823772d87b0da79fa5",
+    "url": "/index.html"
+  }
+];
